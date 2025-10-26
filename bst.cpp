5e00d54a372986b489103bf12c4cee4978a0f40d
brooks_g1 <brooks_g1@denison.edu>
2025-10-26 16:25:25 -0400

    changes to bst.cpp --just starting on it.

--- bst.cpp
+++ bst.cpp
@@ -1,1 +1,53 @@
-//bst.cpp
+//====================================================
+// bst.cpp
+//
+// Created by Gabe, Ramatu, and Phlynn
+//====================================================
+
+#include "iostream"
+#include "hash_table.h"
+#include "string"
+#include "sstream"
+using namespace std;
+
+
+#ifndef BST_CPP
+#define BST_CPP
+
+//===========================================================
+//BST functions
+//===========================================================
+template <class T>
+bst<T>::bst(int m) {
+
+
+}
+
+
+
+//==========================================================
+// empty
+// checks if the bst is empty.
+// 
+// pre-condition: a valid created bst.
+// post-condition: a bool depicting whether the bst is empty
+//==========================================================
+template <class T>
+void bst<T>::empty() {
+
+}
+
+
+//==========================================================
+// insert
+// Inserts key k into a valid bst
+// parameters: T d - element's template data
+//             int k - numeric key value
+// pre-condition: 
+// post-condition: 
+//==========================================================
+template <class T>
+void bst<T>::insert(const T& d, const long&  k) {
+
+}
+
