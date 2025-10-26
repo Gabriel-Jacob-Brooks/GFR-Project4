2035010cb18ff4b67e490aaf83f39db57a2235b5
brooks_g1 <brooks_g1@denison.edu>
2025-10-26 15:37:41 -0400

    changing makefile. And making BST.h

--- bst.h
+++ bst.h
@@ -1,1 +1,40 @@
-///bst.cpp
+// CS271 Binary Search Header File
+
+// Created by Gabe, Ramatu, and Phlynn
+
+
+#ifndef BST_H
+#define BST_H
+
+#include "iostream"
+#include "cmath"
+using namespace std;
+
+//==========================================================
+template <class T>
+class HashTable {
+private:
+    int size; //size of T array 
+    DoublyLinkedList<T>* table;
+
+public:
+    bst(int m); //instantiate
+    ~bst(); // destructor
+    void empty(); // checks if its empty
+    void insert (const T& d, const long&  k);
+    bool get (const long&  k); //returns data associated with key k. // Might be implemented in other DLL???
+    void remove (long k);
+    void max_data();
+    void max_key();
+    void min_data();
+    void min_key();
+    void successor(long k);
+    void in_order();
+    void trim(long low, long high);
+
+
+};
+
+#include "bst.cpp"
+
+#endif
---
+++ makefile
@@ -0,0 +1,20 @@
+all: main
+
+main: hash_table.o dll.o usecase.o main.o
+	g++ -std=c++11 hash_table.o dll.o main.o -o testmain
+
+
+main.o:  main.cpp bst.h
+	g++ -std=c++11 -c main.cpp
+
+
+usecase.o: usecase.cpp bst.h
+	g++ -std=c++11 -c usecase.cpp
+
+
+bst.o: test_bst_example.cpp bst.h
+	g++ -std=c++11 -c test_bst_example.cpp
+
+
+clean:
+	rm -f *.o test
